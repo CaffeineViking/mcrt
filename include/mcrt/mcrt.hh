3e50941a9d5e844d16a4a2ed4121c1ab8e1caecf
Martin Estgren <martin.estgren@gmail.com>
2017-09-06 12:49:42 +0200

    added header for basic ray tracing

--- include/mcrt/mcrt.hh
+++ include/mcrt/mcrt.hh
@@ -1,6 +1,40 @@
 #ifndef MCRT_MCRT_HH
 #define MCRT_MCRT_HH
 
+#include <glm/glm.hpp>
 
+namespace mcrt {    
 
+// Material struct, stores render information about a surface type.
+struct Material {
+    glm::dvec3 color;
+};
+
+// Parametric form representation of a sphere
+struct Sphere {
+    glm::dvec3 origin;
+    double radius;
+    Material material;
+};
+
+struct Ray {
+    glm::dvec3 origin;
+    glm::dvec3 direction;
+};
+
+// 
+struct Triangle {
+    glm::dvec3 v1;
+    glm::dvec3 v2;
+    glm::dvec3 v3;
+    Material material;
+};
+
+// Return distance from ray origin to sphere, -1 means no intersection.
+double sphere_intersect(const Ray&, const Sphere&);
+
+// Returns distance from ray to triangle, -1 means no intersection.
+double triangle_instersect(const Ray&, const Triangle&);
+
+}
 #endif
--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+
 int main(int, char**) {
     std::cout << "Hello, world!"
               << std::endl;
