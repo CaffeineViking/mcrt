b0554fe9c16e462ec6199da6c4119374edd1af88
sciion <martin.estgren@gmail.com>
2017-09-07 23:57:20 +0200

    added ray-triangle intersectiona dn other tatty tat

--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,18 @@
 #include <iostream>
+#include "mcrt/mcrt.hh"
 
 int main(int, char**) {
-    std::cout << "Hello, world!"
-              << std::endl;
+    std::cout << "Hello, world!" << std::endl;
+
+              
+    mcrt::Material dummyMaterial{glm::dvec3(0.0)};
+    mcrt::Ray ray{glm::dvec3(0.0), glm::dvec3(0.0,0.0,1.0)};
+    mcrt::Triangle triangle{
+        glm::dvec3( 5.0, 5.0, 5.0),
+        glm::dvec3(-5.0, 5.0, 5.0),
+        glm::dvec3( 5.0,-5.0, 5.0),
+        dummyMaterial};
+    double distance = mcrt::triangle_instersect(ray,triangle);
+    std::cout << distance << std::endl;
     return 0;
 }
--- src/mcrt/mcrt.cc
+++ src/mcrt/mcrt.cc
@@ -1,1 +1,40 @@
 #include "mcrt/mcrt.hh"
+#include <glm/glm.hpp>
+
+namespace mcrt{
+
+// Return distance from ray origin to sphere, -1 means no intersection.
+double sphere_intersect(const Ray& ray, const Sphere& sphere) {
+    glm::dvec3 a = ray.origin - sphere.origin;
+}
+
+// Returns distance from ray to triangle, 0 means no intersection.
+double triangle_instersect(const Ray& ray, const Triangle& triangle) {
+    glm::dvec3 e1 = triangle.v2 - triangle.v1;
+    glm::dvec3 e2 = triangle.v3 - triangle.v1;
+
+    glm::dvec3 pvec = glm::cross(ray.direction,e2);
+    double det = glm::dot(e1,pvec);
+
+    if(det < 1e-8 && det > -1e-8) {
+        return 0.0;
+    }
+
+    double inv_det = 1.0 / det;
+    glm::dvec3 tvec = ray.origin - triangle.v1;
+    double u = glm::dot(tvec,pvec) * inv_det;
+    if(u < 0 || u > 1) {
+        return 0.0;
+    }
+
+    glm::dvec3 qvec = glm::cross(tvec,e1);
+    double v = glm::dot(ray.direction, qvec) * inv_det;
+    if(v < 0.0 || u + v > 1.0) {
+        return 0.0;
+    }
+
+    return glm::dot(e2,qvec) * inv_det;
+}
+
+
+}
