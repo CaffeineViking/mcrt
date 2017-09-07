9c72f83b0afc08324c639b76c81f1dc2bc4c385c
Erik S. V. Jansson <caffeineviking@gmail.com>
2017-09-07 21:58:22 +0200

    For in-masse includes.

--- include/mcrt/mcrt.hh
+++ include/mcrt/mcrt.hh
@@ -1,6 +1,12 @@
 #ifndef MCRT_MCRT_HH
 #define MCRT_MCRT_HH
 
-
+#include "mcrt/camera.hh"
+#include "mcrt/color.hh"
+#include "mcrt/image_format.hh"
+#include "mcrt/image.hh"
+#include "mcrt/parameters.hh"
+#include "mcrt/param_format.hh"
+#include "mcrt/scene_format.hh"
 
 #endif
