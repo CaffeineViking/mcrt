d0568745770379100e4d2a24b952a0e603b93a1d
Erik S. V. Jansson <caffeineviking@gmail.com>
2017-10-03 22:12:17 +0200

    Cosine-weighted hemisphere sampling.

--- src/mcrt/ray.cc
+++ src/mcrt/ray.cc
@@ -1,25 +1,23 @@
 #include "mcrt/ray.hh"
 
 #include <glm/gtx/rotate_vector.hpp> 
-#include <iostream>
 #include <glm/gtc/constants.hpp>
 
 glm::dvec3 mcrt::Ray::Intersection::sampleHemisphere(const Ray& i) const {
     glm::dvec3 v1 = glm::normalize(i.direction - glm::dot(i.direction, normal) * normal);
     glm::dvec3 v2 = glm::normalize(glm::cross(v1,normal));
 
-    //std::cout << v1.x << " " << v1.y << " " << v1.z << std::endl;
-   // std::cout << v2.x << " " << v2.y << " " << v2.z << std::endl;
-    
     static std::random_device rd;
     static std::mt19937 gen(rd());
     static std::uniform_real_distribution<double> dis(0.0, 1.0);
 
-    double theta1 = dis(gen) * glm::pi<double>() * 2.0;
-    double theta2 = dis(gen) * glm::pi<double>() * 0.5;
-    
-    const glm::dvec3 azimuthRotation = glm::rotate(v1,theta1,normal);
-    const glm::dvec3 inclanationRotation = glm::rotate(v1, theta2,v2);
-    const glm::dvec3 outgoing = glm::normalize(azimuthRotation + inclanationRotation);
+    // Uses the cosine-weighted sampling over the
+    // hemisphere for filter out unimportant rays.
+    double theta = std::asin(std::sqrt(dis(gen)));
+    double phi = dis(gen) * glm::pi<double>() * 2;
+
+    const glm::dvec3 azimuthRotation = glm::rotate(v1, phi, normal);
+    const glm::dvec3 inclinationRotation = glm::rotate(v1, theta, v2);
+    const glm::dvec3 outgoing = glm::normalize(azimuthRotation + inclinationRotation);
     return outgoing;
 }
